8ad5d1f6c93808c273eddca2aa0bacf3bc7dd37f
Charlotte Weager <senwyn@googlemail.com>
2021-05-06 19:37:19 +0100

    Update #1 | Added bolding to cards, changed the font family of headers, and adjusted positioning of buttons

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:name, :rating, :comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
 end
